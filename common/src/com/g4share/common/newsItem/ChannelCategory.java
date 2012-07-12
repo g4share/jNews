4677a232f68c90428853c06f95b89648fd265ff6
g4share <g4share@gmail.com>
2012-07-13 00:50:04 +0300

    changed package name

---
+++ common/src/com/g4share/common/newsItem/ChannelCategory.java
@@ -0,0 +1,37 @@
+package com.g4share.common.newsItem;
+
+import java.io.Serializable;
+
+/**
+ * User: gm
+ */
+public class ChannelCategory implements Serializable, Comparable<ChannelCategory> {
+    private String name;
+    private int count = 0;
+
+    public ChannelCategory(String name) {
+        this.name = name;
+    }
+
+    public void inc(){
+        count++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public int compareTo(ChannelCategory comparedCategory) {
+        if (count == 0) return name.compareTo(comparedCategory.name);
+        return count > comparedCategory.count ? 1 : -1;
+    }
+
+    public void add(int count) {
+        this.count += count;
+    }
+}
---
+++ common/src/com/g4share/common/newsItem/ChannelDescription.java
@@ -0,0 +1,54 @@
+package com.g4share.common.newsItem;
+
+import java.io.Serializable;
+
+/**
+ * User: gm
+ */
+public class ChannelDescription implements Serializable {
+    private String name;
+    private String url;
+    private String date;
+
+    private boolean enabled;
+
+    public ChannelDescription() { }
+
+    public ChannelDescription(String name, String url, boolean enabled) {
+        this.name = name;
+        this.url = url;
+        this.enabled = enabled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
